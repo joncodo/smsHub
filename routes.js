619259732dc52ea99bb9abfac6147bd4fb1fe2bf
joncodo <jonathan.carl.odonnell@gmail.com>
2017-05-10 14:25:04 -0400

    login

--- routes.js
+++ routes.js
@@ -51,7 +51,7 @@ router.post('/login', function(req, res) {
         // ==============================
         var hubAppOptions = {
           method: 'POST',
-          uri: 'https://tranquil-refuge-57483.herokuapp.com/',
+          uri: 'https://tranquil-refuge-57483.herokuapp.com/SMSHub/login',
           body: {
             token: hubLoginToken
           },
